dff95a0bd85731df2675a20bbb8c7c8920949f73
Laura Aponte <lsofia_aponte@javeriana.edu.co>
2026-08-31 01:46:44 -0500

    Create: H and A* functions

--- src/grafo.py
+++ src/grafo.py
@@ -1,18 +1,19 @@
 from collections import deque
+import heapq
 
 
 class Grafo:
 
     def __init__(self, lista_adyacencia):
         self.lista_adyacencia = lista_adyacencia
+        self.meta = None
 
     def obtener_vecinos(self, v):
         return self.lista_adyacencia[v]
 
     # funcion heuristica
     def h(self, n):
-        #inserte su codigo aqui
-        return H[n] # puede retornar una lista con el calculo de la heuristica para cada estado
+        return abs(n[0] - self.meta[0]) + abs(n[1] - self.meta[1]) # puede retornar una lista con el calculo de la heuristica para cada estado
 
     def primero_profundidad(self, nodo_inicio, nodo_final):
         pila = [(nodo_inicio, [nodo_inicio])]
@@ -54,5 +55,30 @@ class Grafo:
         return None
 
     def a_estrella(self, nodo_inicio, nodo_final):
-       #inserte si codigo aqui
+        self.meta = nodo_final
+
+        contador = 0
+        cola_prioridad = [(self.h(nodo_inicio), contador, nodo_inicio, [nodo_inicio], 0)]
+        visitados = set()
+
+        while cola_prioridad:
+            f_actual, _, nodo_actual, camino, g_actual = heapq.heappop(cola_prioridad)
+
+            if nodo_actual == nodo_final:
+                return camino
+
+            if nodo_actual in visitados:
+                continue
+            visitados.add(nodo_actual)
+
+            for vecino, peso in self.obtener_vecinos(nodo_actual):
+                if vecino not in visitados:
+                    g_nuevo = g_actual + peso
+                    f_nuevo = g_nuevo + self.h(vecino)
+                    contador += 1
+                    heapq.heappush(
+                        cola_prioridad,
+                        (f_nuevo, contador, vecino, camino + [vecino], g_nuevo)
+                    )
+
         return None
